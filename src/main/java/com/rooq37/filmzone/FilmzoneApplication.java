d4d7b21921295bd0ef7d40a965713750ccdeb850
Mateusz Rokosa <238501@student.pwr.edu.pl>
2019-07-14 15:20:09 +0200

    initial commit

---
+++ src/main/java/com/rooq37/filmzone/FilmzoneApplication.java
@@ -0,0 +1,13 @@
+package com.rooq37.filmzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmzoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmzoneApplication.class, args);
+    }
+
+}
