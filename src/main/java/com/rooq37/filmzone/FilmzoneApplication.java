44df615747d35f97b1c896142a6215f0258dd8ae
Mateusz Rokosa <238501@student.pwr.edu.pl>
2019-07-16 20:59:45 +0200

    *header *footer

--- src/main/java/com/rooq37/filmzone/FilmzoneApplication.java
+++ src/main/java/com/rooq37/filmzone/FilmzoneApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FilmzoneApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(FilmzoneApplication.class, args);
     }
 
